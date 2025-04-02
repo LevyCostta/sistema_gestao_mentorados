e680e781f8d7f28846e14f888d2724f3ac392a1f
LevyCostta <levicostasouza@hotmail.com>
2025-04-01 22:07:13 -0300

    Inicio ao projeto, criando a pagina inicial(cadastro) e utilizando o banco SQLite3 para armazenamento de dados

---
+++ usuarios/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User 
+# Create your views here.
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=username)
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+
+        User.objects.create_user(
+            username = username,
+            password = senha,
+        )
+        return redirect('/usuarios/login')
