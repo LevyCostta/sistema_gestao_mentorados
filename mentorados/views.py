2c6151863fff4ebc4f384e688b0fbd99b8068dff
LevyCostta <levicostasouza@hotmail.com>
2025-04-02 13:01:59 -0300

    Criado pagina inicial dos mentorados

--- mentorados/views.py
+++ mentorados/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mentorados(request):
-    return HttpResponse('MENTORADOS')
+    if request.method == 'GET':
+        return render(request, 'mentorados.html')
