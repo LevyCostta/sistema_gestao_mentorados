9fc6f982eec7391dcef08cca3aa65f8cb4304522
LevyCostta <levicostasouza@hotmail.com>
2025-04-04 18:38:28 -0300

    Parte de autenticação de token concluida

--- mentorados/views.py
+++ mentorados/views.py
@@ -69,3 +69,14 @@ def reunioes(request):
 def auth(request):
     if request.method == 'GET':
         return render(request, 'auth_mentorado.html')
+    elif request.method == 'POST':
+        token = request.POST.get('token')
+
+        if not Mentorados.objects.filter(token=token).exists():
+            messages.add_message(request, constants.ERROR, 'Token inválido.')
+            return redirect('auth_mentorado')
+        
+        response = redirect('escolher_dia')
+        response.set_cookie('auth_token', token, max_age=3600)
+        return response
+
